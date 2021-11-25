516407b11195661f406518a7e9018003393a1d2e
Luana <luanaff-2008@hotmail.com>
2021-11-24 22:24:36 -0300

    requisito 8 do projeto

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ acessButton.addEventListener("click", function(event){
     let list = document.getElementById("lista-tarefas")
     let itemList = document.createElement("li")
     itemList.innerText=input.value
+    itemList.addEventListener("click",function(event){
+       let selectedItensList = document.querySelectorAll(".color-gray")
+       console.log(selectedItensList)
+       if(selectedItensList.length >= 1){
+         selectedItensList[0].classList.remove("color-gray")
+       }
+        itemList.classList.add('color-gray')
+    });
     list.appendChild(itemList)
     input.value = ""
-    itemList.addEventListener("click",function(event){
-        itemList.style.backgroundColor="rgb(128,128,128)"
-    });
+
  });
 
