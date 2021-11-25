fc46937b91df0d528383706ceebd40159aa03d6f
Luana <luanaff-2008@hotmail.com>
2021-11-24 21:48:23 -0300

    requisito 7 do projeto

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let acessButton = document.querySelector("#criar-tarefa")
 acessButton.addEventListener("click", function(event){
     let input = document.querySelector("#texto-tarefa")
     let list = document.getElementById("lista-tarefas")
-    let ItemList = document.createElement("li")
-    ItemList.innerText=input.value
-    list.appendChild(ItemList)
+    let itemList = document.createElement("li")
+    itemList.innerText=input.value
+    list.appendChild(itemList)
     input.value = ""
+    itemList.addEventListener("click",function(event){
+        itemList.style.backgroundColor="rgb(128,128,128)"
+    });
  });
+
